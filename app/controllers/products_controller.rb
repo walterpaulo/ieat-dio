f7eee4580e5b7da041b06ed593d2634774b59d2d
walterpaulo <walter0paulo@hotmail.com>
2022-01-15 20:21:31 -0300

    add métodos new, create e product_params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      format.html { render: :index, notice: 'Produto criado'}
+    else
+      format.html { render: :new}
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:nome, :value, :restaurant_id)
+  end
 end
