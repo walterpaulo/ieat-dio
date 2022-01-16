1e0a4398cdd28a015642d96637cf03fc5bb80428
walterpaulo <walter0paulo@hotmail.com>
2022-01-16 00:31:27 -0300

    separando permissão de acesso.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -65,6 +65,6 @@ class RestaurantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
-      params.require(:restaurant).permit(:nome, :address)
+      params.require(:restaurant).permit(:nome, :address, :user_id)
     end
 end
